d8117f53df16ef18a0c49c79c0baef353eee62f9
DarisIlham <darisilham32@gmail.com>
2025-06-24 20:46:03 +0700

    fix refresh bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    [react()],
-    tailwindcss(),
-  ],
-})
-
-
-
-
+  plugins: [[react()], tailwindcss()],
+  base: "/", // ini default, pastikan tidak diarahkan ke subfolder aneh
+  build: {
+    rollupOptions: {
+      input: "/index.html",
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
+});
